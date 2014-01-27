57ee2ab920ee9120b6cc6f3dfec1f008d324da77
thomas91310 <thomas.theissier@gmail.com>
2014-01-26 19:05:47 -0600

    Reorganaziation + beginning of UI using bootstrap twitter)

---
+++ js/base.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: thomastheissier
+ * Date: 26/01/2014
+ * Time: 18:09
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function() {
+    $('#modal-box').modal('toggle');
+});
