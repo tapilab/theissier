3642673d407876dcce87bccbf9af0e9aca5c6264
thomas91310 <thomas.theissier@gmail.com>
2014-03-25 12:07:42 -0500

    Script python connects to the database & print the array using method fit_transform of CountVectorizer

--- scriptPredict.py
+++ scriptPredict.py
@@ -2,7 +2,16 @@
 import pymongo
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
+from sklearn.feature_extraction import DictVectorizer
+vec = DictVectorizer()
 db = client.tweetsClassifier
 collection = db['scoredTweets']
+textTweetsArray = []
 for document in collection.find( { }, { "text": 1, "_id": 0 }):
-    print document
+    textTweetsArray.append(document)
+print textTweetsArray
+resultMatrix = vec.fit_transform(textTweetsArray).toarray()
+print ("result matrix : ", resultMatrix)
+# print vec.get_feature_names()
+
+
