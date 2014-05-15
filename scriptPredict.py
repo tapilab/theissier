2afa7a40c869bdcce77d955e5360153967c2f105
thomas91310 <thomas.theissier@gmail.com>
2014-05-15 11:03:01 -0500

    lisibility

--- scriptPredict.py
+++ scriptPredict.py
@@ -16,26 +16,25 @@ class TrainClassifier(object):
         textTweetsArray = []
         sessionNames = []
         scores = []
+        epsilon = 0.44
+        textUnlabledTweets = []
+        idTweetsThatMatchTheCriterion = []
         for document in collection.find({ }, { "text": 1, "id": 1, "score": 1, "sessionname": 1, "_id": 0 }):
             textTweetsArray.append(document['text'])
             sessionNames.append(document['sessionname'])
             scores.append(document['score'])
         resultMatrix = vec.fit_transform(textTweetsArray).toarray()
         clf.fit(resultMatrix, scores)
-        epsilon = 0.44
-        textUnlabledTweets = []
-        idTweetsThatMatchTheCriterion = []
-        print ("len object : ", len(object))
         for i in range(len(object)):
             textUnlabledTweets.append(object[i]['fields']['text'])
         matrixUnlabledTweets = vec.transform(textUnlabledTweets).toarray()
         predictUnlabledTweets = clf.predict_proba(matrixUnlabledTweets)
-        print("predict unlabled tweets : ", predictUnlabledTweets)
+        #print("predict unlabled tweets : ", predictUnlabledTweets)
         for j in range(len(predictUnlabledTweets)):
             if (predictUnlabledTweets[j][1] >= epsilon):
                 idTweetsThatMatchTheCriterion.append(object[j]['_id'])
         #tweets that respect the criterion (more than a certain probability) will be sent to node.js
-        print ("ids : ", idTweetsThatMatchTheCriterion)
+        #print ("ids : ", idTweetsThatMatchTheCriterion)
         return "%s " % idTweetsThatMatchTheCriterion
 s = zerorpc.Server(TrainClassifier())
 s.bind("tcp://127.0.0.1:4242")
